6d04d34c867d8d35a90e78f796eb708c840cdd11
Samuel Josefano Kaloh <kalohsamuel@gmail.com>
2025-09-06 09:01:13 +1000

    update: randomized file picking

--- scenes/game.gd
+++ scenes/game.gd
@@ -9,6 +9,8 @@ extends Node2D
 @onready var file_badge: Control = $FileBadge
 @onready var enemy_hand: Node2D = $EnemyHand
 
+var downloads_path: String = OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS)
+
 func _ready() -> void:
 	toggle_choose.visible = true	
 	choose_okay.visible = false
@@ -29,8 +31,34 @@ func _on_choose_okay_pressed() -> void:
 	var new_move = randi() % enemy_hand.Move.size()
 	enemy_hand.set_move(new_move)
 	
-func _pick_enemy():
-	var fake_file = "test.txt"
-	#var enemy_move = randi() % 3
-	file_badge.set_file(fake_file)
-	#enemy_hand.set_move(enemy_move)
+	_pick_enemy()
+	
+func _list_files(path: String) -> Array:
+	var files: Array = []
+	var da := DirAccess.open(path)
+	if da == null:
+		push_error("Cannot open directory: %s" % path)
+		return files
+
+	da.list_dir_begin()
+	while true:
+		var file_name = da.get_next()
+		if file_name == "": break
+		if da.current_is_dir(): continue
+		if file_name.begins_with("."): continue
+		files.append(file_name)
+	da.list_dir_end()
+	return files
+
+func _pick_enemy() -> void:
+	var files: PackedStringArray = _list_files(downloads_path)
+	if files.is_empty():
+		file_badge.set_file("[empty Downloads]")
+		return
+
+	var idx: int = randi() % files.size()
+	var chosen: String = files[idx]
+	file_badge.set_file(chosen)
+
+	var move: int = randi() % EnemyHand.Move.size()
+	enemy_hand.set_move(move)
